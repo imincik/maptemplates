26e41851eaf48e366cf8745db5933aa5286532f4
Ivan Mincik <ivan.mincik@gmail.com>
2012-09-17 17:07:47 +0200

    Adding possibility to set variables also from command line.

--- mapvars.py
+++ mapvars.py
@@ -1,49 +1,82 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 """UMN Mapserver mapfiles parser with trivial variables support.
 
-Usage mapvars.py mapfile.map.templ > mapfile.map
+Usage: mapvars.py mapfile.map '<key>=<value>;<key>=<value>; ...'
+
+Example: ./mapvars.py test.map.in "USER=MyUser; PASSWORD=MyPassword; SRID=4326; OUTLINECOLOR=100 100 100"
 """
 
 import sys
 import re
 
-vars = {}
 
-def variables(line):
-	varname = line.split('=')[0].strip()
-	varvalue = line.split('=')[1].strip()
-	vars[varname] = varvalue
+kvlocal = {}	# variables from mapfile
+kvcmd = {}	# variables from command line
 
-def parse_line(line):
-	return (line % vars).rstrip()
+def set_var(kvline, kvstore='local'):
+	""" Set variable to 'local' or 'cmd' key-value store. """
 
+	key = kvline.split('=')[0].strip()
+	value = kvline.split('=')[1].strip()
+
+	if kvstore == 'local':
+		kvlocal[key] = value
+	else:
+		kvcmd[key] = value
+
+def get_line(line):
+	""" Return line with variables substituted by values from key-value
+	store. Values from 'cmd' key-value store always overrides values
+	from 'local' one.
+	"""
+
+	kvstore = dict(kvlocal.items() + kvcmd.items())
+	return (line % kvstore).rstrip()
 
 
 if __name__ == '__main__':
+
+	# open mapfile
 	try:
 		filename = sys.argv[1]
+		mapfile = open(filename, 'r')
 	except IndexError:
 		print __doc__
 		sys.exit(1)
 
-	readvars = False
+	# read variables from command line
+	try:
+		for kvline in sys.argv[2].split(';'):
+			set_var(kvline, 'cmd')
+	except IndexError:
+		pass
 
-	file = open(filename, 'r')
-	line = file.readline()
+	varblock = False
+	comment = False
+	line = mapfile.readline()
 	while line:
-		varline = False
-		if re.match('STARTVAR', line.upper().strip()):
-			readvars = True
-			varline = True
-		elif re.match('ENDVAR', line.upper().strip()):
-			readvars = False
-			varline = True
+		if line.strip() != '':	# remove empty lines
 
-		#print readvars, varline, line
-		if readvars == True and varline == False:
-			if not line.strip().startswith('#'):
-				variables(line)
+			keyword = False
+			comment = False
 
-		if readvars == False and varline == False:
-			print parse_line(line)
-		line = file.readline()
+			if re.match('^STARTVAR', line.upper().strip()):
+				keyword = True		# line contains variables declaration keyword
+				varblock = True		# and marks beginning of variables declaration
+			elif re.match('^ENDVAR', line.upper().strip()):
+				keyword = True		# line contains variables declaration keyword
+				varblock = False	# and marks end of variables declaration
+			elif re.match('#', line.strip()):
+				comment = True		# line is comment
+
+			#print '>>>', line.strip(), keyword, varblock, comment
+
+			# set variable if we are in variables declaration block or line is not comment
+			if varblock and not keyword and not comment:
+				set_var(line)
+
+			# print line if we are not in variables declaration block or line is not comment
+			elif not varblock and not keyword and not comment:
+				print get_line(line)
+
+		line = mapfile.readline()
